83f463ecc0a8cb124b9b871f46abb85432bdb538
Don Onserio <donaldonserio20@gmail.com>
2023-09-29 11:20:58 +0300

    ✨ feat!:test structured data on mount

---
+++ pages/cs/index.vue
@@ -0,0 +1,69 @@
+<template>
+<pre>
+    {{ structuredData }}
+</pre>
+  </template>
+  <script setup>
+
+onMounted(()=>{
+    generateStructuredData();
+})
+
+const structuredData = ref('')
+  
+  // Function to generate the JSON-LD script
+  const generateStructuredData = () => {
+    structuredData.value = {
+      "@context": "https://schema.org",
+      "@type": "JobPosting",
+      "title": 'job.value.title',
+      "description": 'job.value.description',
+      "employmentType": 'job.value.employment_type',
+      "jobLocation": {
+        "@type": "Place",
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": 'job.value.job_location',
+          "addressCountry": 'job.value.country',
+        }
+      },
+      "datePosted":' job.value.date_posted',
+      "validThrough": 'job.value.valid_through',
+      "hiringOrganization": {
+        "@type": "Organization",
+        "name": 'job.value.company.name',
+        "sameAs": 'job.value.company.website_uri',
+        "logo": 'job.value.company.logo'
+      },
+      "baseSalary": {
+        "@type": "MonetaryAmount",
+        "currency": 'job.value.currency',
+        "value": {
+          "@type": "QuantitativeValue",
+          "minValue": 'job.value.min_salary',
+          "maxValue": 'job.value.max_salary',
+          "unitText": "MONTH"
+        }
+      },
+      // "responsibilities": "..."
+    };
+  
+    useHead({
+      script: [{
+        type: 'application/ld+json',
+        innerHTML: JSON.stringify({
+          "type": "application/ld+json",
+          "textContent": structuredData.value
+        })
+      }],
+      __dangerouslyDisableSanitizers: ['script'],
+    })
+    console.log('GENERATED STRUCTURED DATA', structuredData.value)
+  }
+  
+  //layout
+  definePageMeta({
+    layout: "accounts",
+  });
+  </script>
+    
